7308f8af6b6622b86e8a5b29b9fd3dcb9e8bc880
h3nrique <henrique.o@ipvc.pt>
2022-11-10 15:00:27 +0000

    update navbar style

--- techquiz/src/Home/NavbarH.js
+++ techquiz/src/Home/NavbarH.js
@@ -6,15 +6,18 @@ import Logo from './Img/TechquizBlack.png'
 const NavBarH = () => {
     return ( 
         <nav className="navbar">
+            <div className='logo'>
                 <img src={Logo}/>
+            </div>
+                
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">Register</a>
-                <a href="/create">Login</a>
+                <a href="/create">Support</a>
+                <a href="/create">AboutUs</a>
+                <a href="/create">Sugestões</a>
             </div>
             <div className="links1">
-                <a href="/create">Register</a>
-                <a href="/create">Register</a>
+                <a href="/login">Login</a>
             </div>
             <div className="profile">
                 <img src={User}/>
