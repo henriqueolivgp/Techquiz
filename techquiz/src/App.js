b266244bc4112ef3d035fa3c7f8d8f44d1ab8878
h3nrique <henrique.o@ipvc.pt>
2022-11-11 23:55:14 +0000

    Update Home Page " Intro and banner done"

--- techquiz/src/App.js
+++ techquiz/src/App.js
@@ -1,13 +1,15 @@
 import Home from './Home/HomePage';
 import './App';
 import NavBarH from './Home/NavbarH';
+import Login from './Login/LoginPage';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
+      <div className='content'>
       <NavBarH/>
       <Home/>
-      <div className='content'>
       </div>
     </div>
   );
--- techquiz/src/Home/HomePage.js
+++ techquiz/src/Home/HomePage.js
@@ -1,12 +1,19 @@
+import img1 from './Img/containerimg.jpg'
+import logo2 from './Img/TechquizBlack.png'
+import './Home.css'
+
 const HomePage = () => {
     return ( 
-        <div className="Page">
-            <div className="content">
-                <tittle>
-                    
-                </tittle>
+        <div class="flex-container">
 
-            </div>
+        <div class="flex-child">
+            <img className='Logo' src={logo2}/>
+            <p className='intro'>Com o Techquiz pode avaliar o  seu conhecimento e saber <br/>o seu nível em todas as diversas áreas de informática.</p>
+        </div>
+  
+        <div class="flex-child green">
+            <img className='banner' src={img1}/>
+        </div>
         </div>
      );
 }
