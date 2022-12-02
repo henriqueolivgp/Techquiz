283c2051f2f67bc663616a80721391d379d1e1de
h3nrique <henrique.o@ipvc.pt>
2022-12-02 01:40:27 +0000

    changed data in database.js

--- techquiz/server/data/context/databasa.js
+++ techquiz/server/data/context/databasa.js
@@ -3,9 +3,9 @@ const Sequelize = require("sequelize");
 const pool = new Sequelize({
   host: "localhost",
   port: "3306",
-  user: "jose",
-  password: "8854",
-  database: "PSI_final",
+  user: "root",
+  password: "",
+  database: "techquiz",
   dialect: "mysql",
 });
 
