3892173519310109161e2010d9fb019d674a1bbc
Henrique Oliveira <101120107+henriqueolivgp@users.noreply.github.com>
2023-02-03 09:08:56 +0000

    Delete techquiz directory

--- techquiz/src/Profile/Profile.js
+++
@@ -1,9 +0,0 @@
-const Profile = () => {
-    return ( 
-        <div className="content">
-            <h2>Profile</h2>
-        </div>
-     );
-}
- 
-export default Profile;
--- techquiz/src/Programacao/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL:"http://localhost:1400/api",
-})
-
-export default api;
